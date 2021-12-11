ceeecb4b70f8578e179da3f9a34a0dd58856e5b5
GW <kowonp@gmail.com>
2021-12-11 19:58:07 +0900

    Change Category

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -9,6 +9,15 @@ const ToDo = ({ id, text, category }: IToDo) => {
     const {
       currentTarget: { name },
     } = event;
+
+    setToDo((todoArray) => {
+      const targetIndex = todoArray.findIndex((todo) => todo.id === id);
+      const frontArray = todoArray.slice(0, targetIndex);
+      const backArray = todoArray.slice(targetIndex + 1);
+      const newTodo = { id, text, category: name as IToDo["category"] };
+      const newTodoArray = [...frontArray, newTodo, ...backArray];
+      return newTodoArray;
+    });
   };
 
   return (
